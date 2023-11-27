3a93c252bc6b424b8167df0b7104ae16d153918b
mark-portnell <147636966+mark-portnell-bsa@users.noreply.github.com>
2023-11-27 08:30:57 +0000

    v3 test ready

--- app/routes.js
+++ app/routes.js
@@ -22,7 +22,45 @@ router.post('/v3/claim-item-start-point', function (req, res) {
       res.redirect('../claims/prototypes/v3/new-claim-item/learner/select-learner.html')
     } else if (startingpoint == "byevent") {
       // Send user to event page
-      res.redirect('../claims/prototypes/v3/new-claim-item/event/select-event.html')
+      res.redirect('../claims/prototypes/v3/new-claim-item/event/select-event-type.html')
+    }
+  
+  })
+
+  router.post('/v3/evidence-answer', function (req, res) {
+
+    // Make a variable and give it the value from 'startingpoint'
+    var evidence = req.session.data['evidence']
+  
+    // Check whether the variable matches a condition
+    if (evidence == "learner-yes"){
+      // Send user to learner page
+      res.redirect('../claims/prototypes/v3/new-claim-item/learner/tu/add-evidence.html')
+    } else if (evidence == "learner-no") {
+      // Send user to event page
+      res.redirect('../claims/prototypes/v3/new-claim-item/learner/tu/check-your-answers.html')
+    } else if (evidence == "event-yes") {
+      // Send user to event page
+      res.redirect('../claims/prototypes/v3/new-claim-item/event/tu/add-evidence.html')
+    } else if (evidence == "event-no") {
+      // Send user to event page
+      res.redirect('../claims/prototypes/v3/new-claim-item/event/tu/check-your-answers.html')
+    }
+  
+  })
+
+  router.post('/v3/event-type-answer', function (req, res) {
+
+    // Make a variable and give it the value from 'startingpoint'
+    var eventtype = req.session.data['event-type']
+  
+    // Check whether the variable matches a condition
+    if (eventtype == "cpd"){
+      // Send user to learner page
+      res.redirect('../claims/prototypes/v3/new-claim-item/event/cpd/select-activity.html')
+    } else if (eventtype == "tu") {
+      // Send user to event page
+      res.redirect('../claims/prototypes/v3/new-claim-item/event/tu/select-course.html')
     }
   
   })
