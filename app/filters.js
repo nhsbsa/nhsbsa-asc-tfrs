45e79d37d057f14a7128ad4e4bcc08d23c5bf230
mark-portnell <147636966+mark-portnell-bsa@users.noreply.github.com>
2024-01-09 15:39:13 +0000

    randomised service name page

--- app/filters.js
+++ app/filters.js
@@ -141,3 +141,13 @@ addFilter('removeSpacesAndLowerCase', function (inputString) {
 
 })
 
+
+addFilter('randomizeOrder', function (array) {
+    // Perform Fisher-Yates shuffle
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+
+  return array;
+})
