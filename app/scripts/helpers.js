d799a0a5451a2ca0ad9d46f5bd67c0916e643fd5
mark-portnell <147636966+mark-portnell-bsa@users.noreply.github.com>
2024-01-15 09:47:56 +0000

    data overflow fix

--- app/scripts/helpers.js
+++ app/scripts/helpers.js
@@ -1,29 +1,29 @@
 function checkClaim(claim) {
     let check = false
-            if ( claim.learners.length>0 && 
-                claim.learners && 
-                claim.startDate !== null && 
-                claim.costPerLearner !== null && 
-                claim.evidenceOfPayment !== null && 
-                claim.learners.every(learner => learner.evidenceOfCompletion !== null) && 
-                ( claim.learners.every(learner => learner.evidenceOfEnrollment !== null) || claim.training.fundingModel == "full") )
+            if ( claim.learners.length>0 &&
+                claim.startDate != null && 
+                claim.costPerLearner != null && 
+                claim.evidenceOfPayment != null && 
+                claim.learners.every(learner => learner.evidence.evidenceOfCompletion != null) && 
+                ( claim.learners.every(learner => learner.evidence.evidenceOfEnrollment != null) || claim.training.fundingModel == "full") )
                 {
                     check = true
             }
             return check
-  }
+}
 
 function updateClaimStatus(claimID, claims) {
     for (const claim of claims) {
         if (claimID == claim.claimID) {
             if (checkClaim(claim))
                 {
+                    console.log('update claim')
                     claim.status = 'ready-to-submit'
             }
             break;
         }
       }
-  }
+}
 
 function removeSpacesAndLowerCase(inputString) {
 // Remove spaces using regular expression
