5d8957a46c7cfcb91f26b4a0f9338822a02c19a5
Mark Portnell <hi.mark.portnell@nhsbsa.nhs.uk>
2024-04-18 17:08:52 +0100

    created claim page and initial layout grid

--- app/views/processing/prototypes/design/v1/filters/filters.js
+++ app/views/processing/prototypes/design/v1/filters/filters.js
@@ -7,3 +7,14 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const addFilter = govukPrototypeKit.views.addFilter
 const { } = require('../helpers/helpers.js');
 const fs = require('fs');
+
+
+addFilter('processorstatusTag_V1', function (statusID) {
+    if (statusID == 'submitted') {
+        return '<strong class="govuk-tag govuk-tag--blue">Not yet processed</strong>'
+    } else if (statusID == 'rejected' || statusID == 'approved') {
+        return '<strong class="govuk-tag govuk-tag--green">Processed</strong>'
+    } else {
+        return '<strong class="govuk-tag govuk-tag--grey">Invalid Status</strong>'
+    }
+}, { renderAsHtml: true })
