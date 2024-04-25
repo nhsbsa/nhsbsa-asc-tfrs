76c25d6b4d15ee8122d09298fe3836ff1b90402c
Hannah Williams <hi.hannah.williams@nhsbsa.nhs.uk>
2024-04-25 13:48:51 +0100

    add validation for unconfirmed declaration

--- app/views/claims/prototypes/design/v9/routes/routes.js
+++ app/views/claims/prototypes/design/v9/routes/routes.js
@@ -34,6 +34,7 @@ router.post('/verify-details-handler', function (req, res) {
   } else {
     res.redirect('account-setup/job-title')
   }
+  delete req.session.data['confirmation'];
 });
 
 
@@ -481,6 +482,10 @@ router.get('/cancel-handler', function (req, res) {
   delete req.session.data['errorFileTooBig'];
   delete req.session.data['errorFileMissing'];
   delete req.session.data['deleteError'];
+  delete req.session.data['jobTitleEmptyError'];
+  delete req.session.data['jobTitle'];
+  delete req.session.data['jobTitleInvalid'];
+  delete req.session.data['declarationSubmitError'];
 
   res.redirect('claim/claim-details' + '?id=' + claimID)
 
@@ -539,18 +544,29 @@ router.post('/create-learner', function (req, res) {
 });
 
 router.post('/validate-job-title', function (req, res) {
-  delete req.session.data.emptyError
-  delete req.session.data.invalid
+  delete req.session.data.jobTitleEmptyError
+  delete req.session.data.jobTitleInvalid
+  delete req.session.data['declarationSubmitError'];
   const jobTitle = req.session.data.jobTitle
 
   var validCharactersRegex = /^[a-zA-Z0-9-]+$/;
 
   if (jobTitle == "") {
-    res.redirect('account-setup/job-title?emptyError=true')
+    res.redirect('account-setup/job-title?jobTitleEmptyError=true')
   } else if (validCharactersRegex.test(jobTitle) == true) {
     res.redirect('account-setup/declaration')
   } else {
-    res.redirect('account-setup/job-title?invalid=true')
+    res.redirect('account-setup/job-title?jobTitleInvalid=true')
+  }
+});
+
+router.post('/declaration-confirmation', function (req, res) {
+  delete req.session.data.declarationSubmitError
+  const declarationConfirmed = req.session.data.declaration
+  if (declarationConfirmed != null) {
+    res.redirect('account-setup/bank-details')
+  } else {
+    res.redirect('account-setup/declaration?declarationSubmitError=true')
   }
 });
 
