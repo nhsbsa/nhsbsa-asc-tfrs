ea535714cce377faad13c8364a5e38baf47e9a9a
Mark Portnell <hi.mark.portnell@nhsbsa.nhs.uk>
2025-06-27 13:34:30 +0100

    removed uneeded filters

--- app/views/processing/v9/_filters/filters.js
+++ app/views/processing/v9/_filters/filters.js
@@ -23,16 +23,6 @@ addFilter('processorstatusTag', function (statusID) {
     }
 }, { renderAsHtml: true })
 
-addFilter('sectionCheck', function (state) {
-    if (state != null) {
-        return "Completed"
-    } else if (state == null) {
-        return '<strong class="govuk-tag govuk-tag--blue">Incomplete</strong>'
-    } else {
-        return "Error"
-    }
-}, { renderAsHtml: true })
-
 addFilter('findClaim', function (claimID, claims) {
     let claim = null;
     for (let c of claims) {
@@ -42,89 +32,6 @@ addFilter('findClaim', function (claimID, claims) {
     }
     return claim;
 })
-
-addFilter('criteriaQuestions', function (criteria, type, claim, header) {
-    if (type == "payment") {
-        switch (criteria) {
-            case "0":
-                if (header == "true") {
-                    return "Course or qualification name"
-                } else {
-                    return "Does the payment evidence show the training was " + claim.training.title + "?"
-                }
-            case "1":
-                if (header == "true") {
-                    return "Training provider or awarding body name"
-                } else {
-                    return "Does the payment evidence show the provider was " + claim.training.awardingOrganisation + "?"
-                }
-            case "2":
-                if (header == "true") {
-                    return "How much was paid"
-                } else {
-                    return "Does the payment evidence show how much was paid?"
-                }
-            case "3":
-                if (header == "true") {
-                    return "When the payment was made"
-                } else {
-                    return "Does the payment evidence show the payment date of  " + formatDate(claim.costDate) + "?"
-                }
-        }
-    } else if (type == "completion") {
-        switch (criteria) {
-            case "0":
-                if (header == "true") {
-                    return "Date the training took place or started"
-                } else {
-                    return "Does the completion evidence show the training start of " + formatDate(claim.startDate) + "?"
-                }
-            case "1":
-                if (header == "true") {
-                    return "Learner’s name"
-                } else {
-                    return "Does the completion evidence show the learner was " + claim.learner.givenName + " " + claim.learner.familyName + "?"
-                }
-            case "2":
-                if (header == "true") {
-                    return "Training provider or awarding body name"
-                } else {
-                    return "Does the completion evidence show the provider was " + claim.training.awardingOrganisation + "?"
-                }
-        }
-    }
-}, { renderAsHtml: true })
-
-addFilter('criteriaAnswer', function (criteria, type, claim) {
-    if (type == "payment") {
-        if (claim.evidenceOfPaymentReview["criteria" + criteria].result) {
-            return "Yes"
-        } else {
-            return "No<br>" + claim.evidenceOfPaymentReview["criteria" + criteria].note
-        }
-    } else if (type == "completion") {
-        if (claim.evidenceOfCompletionReview["criteria" + criteria].result) {
-            return "Yes"
-        } else {
-            return "No<br>" + claim.evidenceOfCompletionReview["criteria" + criteria].note
-        }
-    }
-}, { renderAsHtml: true })
-
-
-addFilter('checkPDF', function (str) {
-    return str.endsWith(".pdf");
-})
-
-addFilter('evidenceBackLink', function (criteriaNo, type) {
-    if (criteriaNo == "1") {
-        return "claim"
-    } else {
-        criteriaNo = String(Number(criteriaNo)+1)
-        return "review-evidence?type=" + type + "&criteria=" + criteriaNo
-    }
-})
-
 
 addFilter('dateSort', function (notes) {
     const sortedData = notes.sort((a, b) => new Date(b.date) - new Date(a.date));
@@ -151,17 +58,6 @@ addFilter('reimbursement', function (claim, paymentReimbursementAmount, training
         return paymentReimbursementAmount
     } else {
         return training.reimbursementAmount
-    }
-});
-
-addFilter('original_reimbursement_amount', function (claim,paymentReimbursementAmount) {
-    if (claim.claimType == "40") {
-        paymentReimbursementAmount = claim.reimbursementAmount
-    }
-    if (paymentReimbursementAmount > claim.training.reimbursementAmount) {
-        return claim.training.reimbursementAmount
-    } else {
-        return paymentReimbursementAmount
     }
 });
 
@@ -196,22 +92,6 @@ addFilter('signatoryErrorMessage', function (submitError) {
     }
     return errorSummaryStr
 }, { renderAsHtml: true });
-
-addFilter('qualificationCheck', function(submission, training, value) {
-    const qualificationsObject = training.find(obj => obj.groupTitle == "Qualifications");
-    let isQualification = false;
-
-    for (let course of qualificationsObject.courses) {
-        if (course.code == submission.trainingCode) {
-            isQualification = true
-        }
-    }
-    if (isQualification) {
-        return value
-    } else {
-        return "Not applicable"
-    }
-})
 
 addFilter('matchPairClaim', function(claimID, claims) {
     var pairID = null
@@ -262,155 +142,6 @@ addFilter('formatInformation', function (foundOrg, enteredInfo, isFromCheckEdite
     return info
 })
 
-addFilter('orderByMostRecent', function (submissions) {
-    let sorted = sortSubmissionsByDate(submissions, 'submittedDate')
-    return sorted
-})
-
-addFilter('createTimelineArray', function (claim, organisations) {
-    // Extract the timestamps and their associated data
-    const events = [];
-    let org = findOrg(organisations, claim.workplaceID)
-    let users = flattenUsers(org)
-
-    let latestSubmission = getMostRelevantSubmission(claim)
-
-    if (claim.claimType == "100" || claim.claimType == "40" ) {
-        // Add completion date
-        events.push({
-            type: "trainingDate",
-            title: "Training completed",
-            date: latestSubmission.completionDate,
-            description: null,
-            link: null,
-            author: null
-        });
-    }
-
-    if (claim.claimType == "100" || claim.claimType == "60" ) { 
-        // Add cost date
-        events.push({
-            type: "trainingDate",
-            title: "Training paid for",
-            date: latestSubmission.costDate,
-            description: null,
-            link: null,
-            author: null
-        });
-
-        // Add start date
-        events.push({
-            type: "trainingDate",
-            title: "Training started",
-            date: latestSubmission.startDate,
-            description: null,
-            link: null,
-            author: null
-        });
-    }
-
-    for (const submission of claim.submissions) {
-
-        events.push({
-            type: "statusDate",
-            title: "Claim submitted",
-            date: submission.submittedDate,
-            description: "View claim",
-            link: "/showClaimHistoryNote?noteType=claim&submittedDate=" + submission.submittedDate,
-            author: findUser(users, submission.submitter) + " (Submitter)"
-        });
-
-        if (
-            ((claim.claimType == "100" || claim.claimType == "60") && submission.evidenceOfPaymentReview.outcome == "fail")
-            || ((claim.claimType == "100" || claim.claimType == "40") && submission.evidenceOfCompletionReview.outcome == "fail")
-            || submission.otherCheck.outcome == "fail"
-        ) {
-
-            // Add rejected date
-            if (submission.processedDate) {
-                events.push({
-                    type: "statusDate",
-                    title: "Claim rejected",
-                    date: submission.processedDate,
-                    description:  "View rejection note",
-                    link: "/showClaimHistoryNote?noteType=rejectionNote&submittedDate=" + submission.submittedDate,
-                    author: "Eren Yeager (Processor)"
-                });
-            }
-        } else if (
-            ((claim.claimType == "100" || claim.claimType == "60") && submission.evidenceOfPaymentReview.outcome == "queried")
-            || ((claim.claimType == "100" || claim.claimType == "40") && submission.evidenceOfCompletionReview.outcome == "queried")
-            || submission.otherCheck.outcome == "queried"
-        ) {
-
-            // Add queried date
-            if (submission.processedDate) {
-                events.push({
-                    type: "statusDate",
-                    title: "Action needed",
-                    date: submission.processedDate,
-                    description: "View actions",
-                    link: "/showClaimHistoryNote?noteType=queryNote&submittedDate=" + submission.submittedDate,
-                    author: "Eren Yeager (Processor)"
-                });
-            }
-
-        } else if (
-            (claim.claimType == "100" && submission.evidenceOfPaymentReview.outcome == "pass" && submission.evidenceOfCompletionReview.outcome == "pass" && submission.otherCheck.outcome == "pass") 
-            || (claim.claimType == "60" && submission.evidenceOfPaymentReview.outcome == "pass" && submission.otherCheck.outcome == "pass")
-            || (claim.claimType == "40" && submission.evidenceOfCompletionReview.outcome == "pass" && submission.otherCheck.outcome == "pass")) {
-
-            // Add approved date
-            if (submission.processedDate) {
-                events.push({
-                    type: "statusDate",
-                    title: "Claim approved",
-                    date: submission.processedDate,
-                    description:  null,
-                    link: null,
-                    author: "Eren Yeager (Processor)"
-                });
-            }
-
-        } 
-    }
-
-    // Add created date
-    if (claim.createdDate) {
-        events.push({
-            type: "statusDate",
-            title: "Claim created",
-            date: claim.createdDate,
-            description: null,
-            link: null,
-            author: findUser(users, claim.createdBy) + " (Submitter)"
-        });
-    }
-
-    // Sort the events by date in descending order
-    events.sort((a, b) => new Date(b.date) - new Date(a.date));
-
-    return events;
-})
-
-addFilter('countOccurrences', function (events,string) {
-     // Validate input
-    if (!Array.isArray(events)) {
-        throw new Error("The first argument must be an array.");
-    }
-    if (typeof string !== "string") {
-        throw new Error("The second argument must be a string.");
-    }
-
-    // Count the objects
-    return events.reduce((count, obj) => {
-        if (string === "all" || obj.type === string) {
-            count++;
-        }
-        return count;
-    }, 0);
-})
-
 addFilter('findOrgClaims', function (orgID, claims) {
     let orgClaims = [];
     for (const claim of claims) {
@@ -424,19 +155,6 @@ addFilter('findOrgClaims', function (orgID, claims) {
 addFilter('pageCount', function (content, perPage) {
     return Math.ceil(content / perPage)
 })
-
-addFilter('typeTag', function (type) {
-    switch (type) {
-        case null:
-            return ""
-        case "100":
-            return '<strong class="govuk-tag govuk-tag--orange">100</strong>'
-        case "60":
-            return '<strong class="govuk-tag govuk-tag--yellow">60</strong>'
-        case "40":
-            return '<strong class="govuk-tag govuk-tag--purple">40</strong>'
-    }
-}, { renderAsHtml: true })
 
 addFilter('orderClaims', function (claims) {
     
@@ -484,28 +202,6 @@ addFilter('matchResend', function (resendList, email) {
     return false
 })
 
-addFilter('removeClaimSuffix', function (claimID) {
-    // Check if the string has at least two characters
-    if (claimID.length < 2) {
-        return ''; // Return an empty string if there are less than two characters
-    }
-    // Use the slice method to remove the last two characters
-    return claimID.slice(0, -2);
-})
-
-addFilter('typeTag', function (type) {
-    switch (type) {
-        case null:
-            return ""
-        case "100":
-            return '<strong class="govuk-tag govuk-tag--orange">100</strong>'
-        case "60":
-            return '<strong class="govuk-tag govuk-tag--yellow">60</strong>'
-        case "40":
-            return '<strong class="govuk-tag govuk-tag--purple">40</strong>'
-    }
-}, { renderAsHtml: true })
-
 addFilter('parseInt', function(value, radix = 10) {
     return parseInt(value, radix);
 });
@@ -513,17 +209,6 @@ addFilter('parseInt', function(value, radix = 10) {
 addFilter('min', (value1, value2) => {
     return Math.min(value1, value2);
 });
-
-
-addFilter('processedDate', function (claim) {
-    let processedDate = null
-    if (claim.approvedDate) {
-        processedDate = claim.approvedDate
-    } else if (claim.rejectedDate) {
-        processedDate = claim.rejectedDate
-    }
-    return processedDate
-})
 
 addFilter('sortByFirstName', function (inactiveClaims) {
     return inactiveClaims.sort((a, b) => {
@@ -539,17 +224,6 @@ addFilter('findLearner', (learnerID, learners) => {
     return findLearnerById(learnerID, learners)
 })
 
-addFilter('trainingTypeCheck', function (trainingCode, trainingList, matchType) {
-
-    for (let trainingGroup of trainingList) {
-        for (let training of trainingGroup.courses) {
-            if (trainingCode == training.code) {
-                return trainingGroup.groupTitle == matchType;
-            }
-        }
-    }
-})
-
 addFilter('findUser', function (email, org) {
     users = flattenUsers(org)
     let user = null;
@@ -560,7 +234,6 @@ addFilter('findUser', function (email, org) {
     }
     return user;
 })
-
 
 addFilter('findSubmissionByDate', function (submissions, submittedDate) {
     const submission = submissions.find(s => s.submittedDate == submittedDate);
