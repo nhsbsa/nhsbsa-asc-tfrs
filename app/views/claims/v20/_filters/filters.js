46792b0d5d4cd29cd582810462848f5fb0bd0881
mark-portnell-bsa <hi.mark.portnell@nhsbsa.nhs.uk>
2025-11-20 09:16:24 +0000

    added scenario data

--- app/views/claims/v20/_filters/filters.js
+++ app/views/claims/v20/_filters/filters.js
@@ -1136,10 +1136,13 @@ addFilter('getNote', function (learnerID, claim, pairClaim) {
         submission = getMostRelevantSubmission(claim)
     }
     draftSubmission = getDraftSubmission(claim)
-    const result = draftSubmission.learners.find(item => item.learnerID === learnerID);
-    if (result != null && result.learnerChanged != null) {
-        learnerID = result.learnerChanged
+    if (draftSubmission != null) {
+        const result = draftSubmission.learners.find(item => item.learnerID === learnerID);
+        if (result != null && result.learnerChanged != null) {
+            learnerID = result.learnerChanged
+        }
     }
+    
 
     const learner = submission.learners.find(l => l.learnerID === learnerID);
     
