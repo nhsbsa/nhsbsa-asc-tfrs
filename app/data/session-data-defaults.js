595b668e1b72a5e88687a9ebf9ecd68fa880b5c3
mark-portnell-bsa <hi.mark.portnell@nhsbsa.nhs.uk>
2026-02-09 12:58:01 +0000

    added v22 claims prototype

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -3,6 +3,16 @@ module.exports = {
         "claimsCurrent": "v21",
         "claimVersions": [
             {
+                "versionNo": "22",
+                "phase": "Beta",
+                "status": ["Design"],
+                "designHistory": "https://asc-tfrs-design-history-689d381cad9a.herokuapp.com/tags/claims-version-22/",
+                "tested": null,
+                "created": null,
+                "aims": null,
+                "learnt": null
+            },
+            {
                 "versionNo": "21",
                 "phase": "Beta",
                 "status": ["UR"],
